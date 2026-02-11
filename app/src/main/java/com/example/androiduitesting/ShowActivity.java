a016b07d55eba1730de1449150d37cea9933251c
Dammy <olofinti@ualberta.ca>
2026-02-10 20:17:20 -0700

    Commit 1

---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,31 @@
+package com.example.androiduitesting;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        TextView textViewCityName = findViewById(R.id.textView_cityName);
+        Button buttonBack = findViewById(R.id.button_back);
+
+        String cityName = getIntent().getStringExtra("CITY_NAME");
+        if (cityName != null) {
+            textViewCityName.setText(cityName);
+        }
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
